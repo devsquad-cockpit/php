23e6d61db33295dff65c3a56bb0e4b85e9b0f449
Hecktor Viegas <hecktorvn@hotmail.com>
2022-09-09 14:15:31 -0300

    COC-102: Prepare the tests

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cockpit\Php\Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    protected $loadEnvironmentVariables = false;
+}
