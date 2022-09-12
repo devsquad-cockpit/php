9c193b78240cb67f45aecd41a26adbb554477939
Hecktor Viegas <hecktorvn@hotmail.com>
2022-09-12 11:17:38 -0300

    COC-101: Remove unused functions

--- src/Exceptions/CockpitErrorHandler.php
+++ src/Exceptions/CockpitErrorHandler.php
@@ -5,53 +5,11 @@ namespace Cockpit\Php\Exceptions;
 use Cockpit\Php\Context\StackTraceContext;
 use Exception;
 use GuzzleHttp\Client;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
-use InvalidArgumentException;
-use Monolog\Logger;
 use Throwable;
 
 class CockpitErrorHandler
 {
-    protected int $minimumLogLevel = Logger::ERROR;
-
-    public function setMinimumLogLevel(int $level)
-    {
-        if (!in_array($level, Logger::getLevels())) {
-            throw new InvalidArgumentException('The given log level is not supported');
-        }
-
-        $this->minimumLogLevel = $level;
-    }
-
-    public function write(array $record): void
-    {
-        if (!$this->shouldReport($record)) {
-            return;
-        }
-
-        $this->log(
-            $record['context']['exception'],
-            Arr::except($record['context'], 'exception')
-        );
-    }
-
-    protected function shouldReport(array $report): bool
-    {
-        return $this->hasException($report) && $this->hasValidLogLevel($report);
-    }
-
-    protected function hasException(array $report): bool
-    {
-        return isset($report['context']['exception'])
-               && $report['context']['exception'] instanceof Throwable;
-    }
-
-    protected function hasValidLogLevel(array $report): bool
-    {
-        return $report['level'] >= $this->minimumLogLevel;
-    }
-    
     public static function log(Throwable $throwable)
     {
         $traceContext = new StackTraceContext($throwable);
