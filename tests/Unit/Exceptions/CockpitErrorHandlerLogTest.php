7393bc2f19cd5c080e99a0648c6221151c283ce1
Hecktor Viegas <hecktorvn@hotmail.com>
2022-09-12 11:15:01 -0300

    COC-101: Improve test

--- tests/Unit/Exceptions/CockpitErrorHandlerLogTest.php
+++ tests/Unit/Exceptions/CockpitErrorHandlerLogTest.php
@@ -1,20 +1,15 @@
 <?php
 
 use Cockpit\Php\Exceptions\CockpitErrorHandler;
-use Cockpit\Php\Tests\Fixtures\Services\MyService;
+use Cockpit\Php\Tests\Fixtures\Exceptions\MyException;
 use GuzzleHttp\Client;
 
 it('should send error to cockpit server', function () {
     $this->setRunTestInSeparateProcess(true);
 
-    try {
-        (new MyService())->handle();
-    } catch (Exception $e) {
-    }
-
     mock('overload:' . Client::class)
         ->shouldReceive('post')
         ->times(1);
 
-    CockpitErrorHandler::log($e);
+    CockpitErrorHandler::log(new MyException());
 });
