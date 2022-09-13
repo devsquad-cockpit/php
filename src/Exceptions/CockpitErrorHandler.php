a70dbdf290d7933eefe19da7d197bb0df971701d
Hecktor Viegas <hecktorvn@hotmail.com>
2022-09-13 14:09:47 -0300

    COC-101: Improve payload

--- src/Exceptions/CockpitErrorHandler.php
+++ src/Exceptions/CockpitErrorHandler.php
@@ -2,9 +2,11 @@
 
 namespace Cockpit\Php\Exceptions;
 
+use Cockpit\Php\Common\OccurrenceType;
 use Cockpit\Php\Context\StackTraceContext;
 use Exception;
 use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Throwable;
 
@@ -15,17 +17,33 @@ class CockpitErrorHandler
         $traceContext = new StackTraceContext($throwable);
 
         $data = [
-            'throwable' => [
-                'exception'   => Str::replace('Symfony\\Component\\ErrorHandler\\', '', get_class($throwable)),
-                'message'     => $throwable->getMessage(),
-                'file'        => $throwable->getFile(),
-                'code'        => $throwable->getCode(),
-                'resolved_at' => null,
-            ],
-            'traceContext' => $traceContext->getContext(),
+            'exception'   => Str::replace('Symfony\\Component\\ErrorHandler\\', '', get_class($throwable)),
+            'message'     => $throwable->getMessage(),
+            'file'        => $throwable->getFile(),
+            'code'        => $throwable->getCode(),
+            'resolved_at' => null,
+            'type'        => self::getExceptionType(),
+            'url'         => self::resolveUrl(),
+            'trace'       => $traceContext->getContext(),
         ];
 
         self::send($data);
+    }
+
+    protected static function resolveUrl(): ?string
+    {
+        return !runningInConsole()
+            ? Request::createFromGlobals()->fullUrl()
+            : null;
+    }
+
+    protected static function getExceptionType(): string
+    {
+        if (!php_sapi_name() == "cli") {
+            return OccurrenceType::WEB;
+        }
+
+        return OccurrenceType::CLI;
     }
 
     protected static function send($data)
