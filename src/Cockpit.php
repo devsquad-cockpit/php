2f91bed4499afcd02d7f61b143b0b0f0ed2143a8
Hecktor Viegas <hecktorvn@hotmail.com>
2022-09-13 10:51:26 -0300

    COC-117: AddFunction to add custom envs

--- src/Cockpit.php
+++ src/Cockpit.php
@@ -3,4 +3,37 @@ namespace Cockpit\Php;
 
 class Cockpit
 {
+    protected static $addEnvCallbacks = [];
+    public static $frameworkVersion;
+
+    public static function addEnvs($callback)
+    {
+        if (is_callable($callback)) {
+            self::$addEnvCallbacks[] = $callback;
+        }
+
+        if (is_array($callback)) {
+            self::$addEnvCallbacks[] = function () use ($callback) {
+                return $callback;
+            };
+        }
+    }
+
+    public static function getEnvs()
+    {
+        $envs = [];
+
+        foreach (self::$addEnvCallbacks as $func) {
+            $envs = array_merge($envs, ($func)());
+        }
+
+        return $envs;
+    }
+
+    public function frameworkVersion(string $frameworkVersion)
+    {
+        $this->frameworkVersion = $frameworkVersion;
+
+        return $this;
+    }
 }
