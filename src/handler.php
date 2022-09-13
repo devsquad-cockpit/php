313261dec9a0a7316f2fb50475f5e5672ee30982
Hecktor Viegas <hecktorvn@hotmail.com>
2022-09-13 07:54:22 -0300

    COC-107: Start the Dump Context

--- src/handler.php
+++ src/handler.php
@@ -1,7 +1,9 @@
 <?php
 
+use Cockpit\Php\Context\DumpContext;
 use Cockpit\Php\Exceptions\CockpitErrorHandler;
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
+DumpContext::start();
 $handler = ErrorHandler::register();
 $handler->setExceptionHandler([CockpitErrorHandler::class, 'log']);
