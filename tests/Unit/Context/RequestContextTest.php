e9e1343b0f6271fc27d29080efa83b99e2159b98
Hecktor Viegas <hecktorvn@hotmail.com>
2022-09-14 11:59:43 -0300

    COC-114: Add test to Request Context

---
+++ tests/Unit/Context/RequestContextTest.php
@@ -0,0 +1,352 @@
+<?php
+
+namespace Cockpit\Php\Tests\Unit\Context;
+
+use Cockpit\Php\Context\RequestContext;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Mockery;
+use Mockery\MockInterface;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Exception\InvalidArgumentException;
+
+class RequestContextMock extends RequestContext
+{
+    public function __construct(
+        $request,
+        array $hideFromRequest = [],
+        array $hideFromHeaders = [],
+        array $hideFromCookies = []
+    ) {
+        $this->request = $request;
+
+        $this->hideFromRequest = $hideFromRequest;
+        $this->hideFromHeaders = $hideFromHeaders;
+        $this->hideFromCookies = $hideFromCookies;
+    }
+}
+
+it('should retrieve basic request data', function () {
+    $appSession = 'eyJpdiI6IkRIQU1CUHhLS3loNlU5VzNsUHZRcnc9PSIsInZhbHVlIjoiRW5zbnI5N0F0eGQ1dGxmV2h6OU9Ddz09IiwibWFjIjoiZWFmMGZiODUwMWQxY2IzNjI5OGUyYTU1NjUwNDUyZDNiZDk4NjY5YTk5OTk5MTUyZjNmNzI3NmE3NWRhNjcxNCIsInRhZyI6IiJ9';
+
+    $request = Request::create(
+        '/update',
+        'PUT',
+        [],
+        ['app_session' => $appSession],
+        [],
+        ['HTTP_ACCEPT' => 'application/json']
+    );
+
+    $context = (new RequestContextMock($request))->getContext();
+
+    expect($context)
+        ->toBeArray()
+        ->not->toBeEmpty()
+        ->and($context['request']['url'])->toBe('http://localhost/update')
+        ->and($context['request']['method'])->toBe('PUT')
+        ->and($context['headers']['accept'][0])->toBe('application/json')
+        ->and($context['query_string'])->toBeArray()->toBeEmpty()
+        ->and($context['body'])->toBeArray()->toBeEmpty()
+        ->and($context['files'])->toBeArray()->toBeEmpty()
+        ->and($context['cookies'])->toBeInstanceOf(Collection::class)
+        ->and($context['cookies']['app_session'])->toBe($appSession);
+});
+
+it('should test if payload will comes with query string', function () {
+    $request = Request::create('/update?only_active=1', 'PUT');
+    $context = (new RequestContextMock($request))->getContext();
+
+    expect($context['request']['url'])->toBe('http://localhost/update')
+        ->and($context['request']['method'])->toBe('PUT')
+        ->and($context['query_string'])->toBe(['only_active' => '1']);
+});
+
+it('should test if payload will comes with body content', function () {
+    $request = Request::create('/update', 'PUT');
+    $request->request->add([
+        'name'      => 'John Doe',
+        'is_active' => false,
+    ]);
+
+    $context = (new RequestContextMock($request))->getContext();
+
+    expect($context['request']['url'])->toBe('http://localhost/update')
+        ->and($context['request']['method'])->toBe('PUT')
+        ->and($context['body'])->toBeArray()->toBe([
+            'name'      => 'John Doe',
+            'is_active' => false,
+        ]);
+});
+
+it('should test if files are present on payload', function () {
+    $file = UploadedFile::fake()->image('avatar.png');
+
+    $request = Request::create('/update', 'PUT', [], [], [
+        'avatar' => $file,
+    ]);
+
+    $context = (new RequestContextMock($request))->getContext();
+
+    expect($context['files'])->toBe([
+        'avatar' => [
+            'pathname' => $file->getPathname(),
+            'size'     => $file->getSize(),
+            'mimeType' => $file->getMimeType(),
+        ],
+    ]);
+});
+
+it('should return an empty array if files arent an instance of UploadedFile', function () {
+    $request = Request::create('/update', 'PUT', [], [], [
+        'avatar' => [],
+    ]);
+
+    $context = (new RequestContextMock($request))->getContext();
+
+    expect($context['files'])->toBe([
+        'avatar' => [],
+    ]);
+});
+
+it(
+    'it should return default file values if exception occurs',
+    function (
+        $method,
+        $exception,
+        $size,
+        $mime
+    ) {
+        $file = Mockery::mock(
+            UploadedFile::class,
+            function (MockInterface $mock) use ($method, $exception, $size, $mime) {
+                $mock->shouldReceive('getPathname')->andReturn('/some/string');
+
+                if ($method == 'getSize') {
+                    $mock->shouldReceive('getMimeType')->andReturn($mime);
+                } else {
+                    $mock->shouldReceive('getSize')->andReturn($size);
+                }
+
+                $mock->shouldReceive($method)->andThrow($exception);
+            }
+        )->makePartial();
+
+        $request = Request::create('/update', 'PUT', [], [], [
+            'avatar' => $file,
+        ]);
+
+        $context = (new RequestContextMock($request))->getContext();
+
+        expect($context['files'])->toBe([
+            'avatar' => [
+                'pathname' => '/some/string',
+                'size'     => $size,
+                'mimeType' => $mime,
+            ],
+        ]);
+    }
+)->with([
+    ['getSize', RuntimeException::class, 0, 'image/jpg'],
+    ['getMimeType', InvalidArgumentException::class, 1024, 'undefined'],
+]);
+
+it('should check cURL command', function () {
+    $appSession = 'eyJpdiI6IkRIQU1CUHhLS3loNlU5VzNsUHZRcnc9PSIsInZhbHVlIjoiRW5zbnI5N0F0eGQ1dGxmV2h6OU9Ddz09IiwibWFjIjoiZWFmMGZiODUwMWQxY2IzNjI5OGUyYTU1NjUwNDUyZDNiZDk4NjY5YTk5OTk5MTUyZjNmNzI3NmE3NWRhNjcxNCIsInRhZyI6IiJ9';
+
+    $request = Request::create(
+        '/update',
+        'PUT',
+        [],
+        ['app_session' => $appSession],
+    );
+
+    $request->request->add(['name' => 'John Doe', 'is_active' => false]);
+
+    $context = (new RequestContextMock($request))->getContext();
+
+    $headers = "";
+
+    foreach ($request->headers->all() as $header => $value) {
+        $value = implode(',', $value);
+        $headers .= "\t-H '{$header}: {$value}' \ \r\n";
+    }
+
+    $body    = "";
+    $allBody = $request->request->all();
+    $lastKey = array_key_last($allBody);
+
+    foreach ($allBody as $label => $value) {
+        $body .= "\t-F '{$label}={$value}'";
+
+        if ($label != $lastKey) {
+            $body .= " \ \r\n";
+        }
+    }
+
+    expect($context['request']['curl'])
+        ->toBe(
+            <<<SHELL
+    curl "http://localhost/update" \
+    -X PUT \
+{$headers}{$body}
+SHELL
+        );
+});
+
+it('should check cURL command when application is working with json', function () {
+    $request = Request::create(
+        '/update',
+        'PUT',
+        [],
+        [],
+        [],
+        ['CONTENT_TYPE' => 'application/json']
+    );
+
+    $request->request->add(['name' => 'John Doe', 'is_active' => false]);
+    $context = (new RequestContextMock($request))->getContext();
+
+    $headers = "";
+
+    foreach ($request->headers->all() as $header => $value) {
+        $value = implode(',', $value);
+        $headers .= "\t-H '{$header}: {$value}' \ \r\n";
+    }
+
+    $body = "\t-d '" . json_encode(['name' => 'John Doe', 'is_active' => false]) . "' \ \r\n";
+
+    expect($context['request']['curl'])
+        ->toBe(
+            <<<SHELL
+    curl "http://localhost/update" \
+    -X PUT \
+{$headers}{$body}
+SHELL
+        );
+});
+
+it('should return a empty session collection if app is running in console', function () {
+    $request = Request::create('/users');
+    
+    $context = (new RequestContextMock($request))->getContext();
+
+    expect($context['session'])->toBeEmpty();
+});
+
+it('should hide sensitive data from request with default values', function () {
+    $request = Request::create('/create', 'POST');
+    $request->request->add([
+        'name'     => 'Some user name',
+        'email'    => 'user@email.com',
+        'password' => 'password',
+    ]);
+
+    $context = (new RequestContextMock($request, ['password']))->getContext();
+
+    expect($context['body']['password'])
+        ->toBe('*****');
+});
+
+it('should hide sensitive data from request with new defined values', function () {
+    $request = Request::create('/create', 'POST');
+    $request->request->add([
+        'name'    => 'Some user name',
+        'email'   => 'user@email.com',
+        'api_key' => Str::random(),
+    ]);
+
+    $context = (new RequestContextMock($request, ['api_key']))->getContext();
+
+    expect($context['body']['api_key'])
+        ->toBe('*****');
+});
+
+it('should hide sensitive data from a multidimensional array', function () {
+    $request = Request::create('/create', 'POST', [], [], [], ['CONTENT_TYPE' => 'application/json']);
+    $request->request->add([
+        'user' => [
+            'name'     => 'Some user name',
+            'password' => 'password',
+        ],
+    ]);
+
+    $context = (new RequestContextMock($request, ['user.password']))->getContext();
+
+    expect($context['body']['user']['name'])
+        ->toBe('Some user name')
+        ->and($context['body']['user']['password'])
+        ->toBe('*****');
+});
+
+it('should hide headers from request with default cockpit values', function () {
+    $request = Request::create('/update', 'PUT');
+    $request->headers->set('Authorization', 'Bearer ' . Str::random());
+
+    $context = (new RequestContextMock($request, [], ['authorization']))->getContext();
+
+    expect($context['headers']['authorization'])
+        ->toBe(['*****']);
+});
+
+it('should hide headers from request with values defined by user', function () {
+    $request = Request::create('/update', 'PUT');
+    $request->headers->set('X-Client-Id', Str::random());
+
+    $context = (new RequestContextMock($request, [], ['x-client-id']))->getContext();
+
+    expect($context['headers']['x-client-id'])
+        ->toBe(['*****']);
+});
+
+it('should hide coockies from request with values defined by user', function () {
+    $request = Request::create('/update', 'PUT');
+    $request->cookies->set('X-Client-Id', Str::random());
+    
+    $context = (new RequestContextMock($request, [], [], ['x-client-id']))->getContext();
+
+    expect($context['cookies']->first())
+        ->toBe('*****');
+});
+
+it('should check if cURL command will hide headers', function () {
+    $request = Request::create('/update', 'PUT');
+
+    $request->request->add(['name' => 'John Doe', 'is_active' => 0]);
+    $request->headers->set('Authorization', 'Basic ZGV2c3F1YWQ6MTIzNDU2');
+
+    $context = (new RequestContextMock($request, [], ['authorization']))->getContext();
+
+    $headers = "";
+
+    foreach ($request->headers->all() as $header => $value) {
+        $value = $header == 'authorization'
+            ? '*****'
+            : implode(',', $value);
+
+        $headers .= "\t-H '{$header}: {$value}' \ \r\n";
+    }
+
+    $body    = "";
+    $allBody = $request->request->all();
+    $lastKey = array_key_last($allBody);
+
+    foreach ($allBody as $label => $value) {
+        $body .= "\t-F '{$label}={$value}'";
+
+        if ($label != $lastKey) {
+            $body .= " \ \r\n";
+        }
+    }
+
+    expect($context['request']['curl'])
+        ->toBe(
+            <<<SHELL
+    curl "http://localhost/update" \
+    -X PUT \
+{$headers}{$body}
+SHELL
+        );
+});
